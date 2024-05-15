a621d3e1d30031408ecd2f246b213e6546f11889
Devroop Saha <130696540+devroopsaha744@users.noreply.github.com>
2024-05-15 09:26:50 +0530

    streamlit app

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import generation
+
+chain  = generation.rag_chain
+
+
+def generate_response(text):
+    response = chain.invoke(text)
+    return response
+
+st.title("Chikitsak")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Hi, How can I help you?"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = generate_response(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
